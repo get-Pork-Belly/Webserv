b9d9769c5eda979ba3e741ce16e5d83e8dc45006
humblEgo <injun.woo30000@gmail.com>
2020-10-14 00:14:37 +0900

    MODIFY: ltrim, rtrim

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sanam <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: iwoo <iwoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 09:51:39 by sanam             #+#    #+#             */
-/*   Updated: 2020/10/13 15:46:48 by sanam            ###   ########.fr       */
+/*   Updated: 2020/10/14 00:05:55 by iwoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,43 @@
 
 namespace ft
 {
-	std::vector<std::string> split(std::string str, std::string const &token)
-	{
-		std::vector<std::string>	result;
-		int							index;
+std::vector<std::string> split(std::string str, std::string const &token)
+{
+	std::vector<std::string>	result;
+	int							index;
+	std::string 				tmp;
 
-		while (str.size())
+	while (str.size())
+	{
+		index = str.find(token);
+		if (static_cast<size_t>(index) != std::string::npos)
 		{
-			index = str.find(token);
-			if (index != std::string::npos)
-			{
-				result.push_back(str.substr(0, index));
-				str = str.substr(index + token.size());
-				if (str.size() == 0)
-					result.push_back(str);
-			}
-			else
-			{
-				result.push_back(str);
-				str = "";
-			}
+			tmp = str.substr(0, index);
+			if (tmp.size())
+				result.push_back(tmp);
+			str = str.substr(index + token.size());
+			// if (str.size() == 0)
+			// 	result.push_back(str);
 		}
-		return (result);
+		else
+		{
+			result.push_back(str);
+			str = "";
+		}
 	}
+	return (result);
+}
 
-	std::string ltrim(const std::string& str, const char *token="\t\n ")
-	{
-		size_t n = str.find_first_not_of(token);
-		return (n == std::string::npos ? str : str.substr(n, str.length()));
-	}
+std::string ltrim(const std::string& str, const char *token)
+{
+	size_t n = str.find_first_not_of(token);
+	return (n == std::string::npos ? str : str.substr(n, str.length()));
+}
 
-	std::string rtrim(const std::string& str, const char *token="\t\n ")
-	{
-		size_t n = str.find_last_not_of(token);
-		return (n == std::string::npos ? str : str.substr(0, n + 1));
-	}
+std::string rtrim(const std::string& str, const char *token)
+{
+	size_t n = str.find_last_not_of(token);
+	return (n == std::string::npos ? str : str.substr(0, n + 1));
+}
+
 }
