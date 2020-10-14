1ec43b71fb83b2354cecf101bc13085dac5822e6
iwoo <iwoo@student.42seoul.kr>
2020-10-14 09:58:15 +0900

    TEST: split_test case
    added test cases-newline, empty string

--- tests/split_test.cpp
+++ tests/split_test.cpp
@@ -3,9 +3,10 @@
 
 int main(void)
 {
+
+std::vector<std::string> res;
+{
     std::string test1 = "   abc def ghi  i   ";
-    // expected: [abc] [def] [ghi] [i]
-    std::vector<std::string> res;
 
     res = ft::split(test1, std::string(" "));
 
@@ -13,15 +14,43 @@ int main(void)
     {
         std::cout<<i<<std::endl;
     }
+}
 
+{
     std::cout<<"==============================="<<std::endl;
     std::string test2 = " abc adb abg abf abh ";
-    //expected: [ ] [c adb ] [g ] [f ] [h ]
     res = ft::split(test2, "ab");
 
     for (auto& i : res)
     {
         std::cout<<i<<std::endl;
     }
+}
+
+{
+    std::cout<<"==============================="<<std::endl;
+    std::string test = " server {\n abc\n adb\n abg\n abf\n abh\n}\n";
+    res = ft::split(test, "\n");
+
+    for (auto& i : res)
+    {
+        std::cout<<i<<std::endl;
+    }
+}
+
+{
+    std::cout<<"==============================="<<std::endl;
+    std::cout<<"blank case"<<std::endl;
+    std::string test = "";
+    res = ft::split(test, "\n");
+
+    for (auto& i : res)
+    {
+        std::cout<<i<<std::endl;
+    }
+}
+
+
+
     return (0);
 }
