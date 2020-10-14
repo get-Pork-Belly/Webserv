45c633c9a0e918e66de3b32b942df730bda57590
iwoo <iwoo@student.42seoul.kr>
2020-10-14 13:29:40 +0900

    FEAT: httpParseBlock declaration
    Delete Struct global_config and new method httpParseBlock

--- include/ServerGenerator.hpp
+++ include/ServerGenerator.hpp
@@ -15,7 +15,6 @@ private:
 
 private:
     const ServerManager&		_server_manager;
-    struct GlobalConfig			_global_config;
     std::vector<std::string>	_configfile_lines;
 
 public:
@@ -32,14 +31,14 @@ public:
     /* Exception */
     /* Util */
     void convertFileToStringVector(const char *config_file_path);
-    bool isValidConfigFile();
     // TODO isValidConfigFile 구현하기
     // bool isValidConfigFile() const; //throw
     void setGlobalConfig();
-    void generateServers(std::vector<std::string>& servers);
+    void generateServers(std::vector<Server *>& servers);
     // NOTE it를 레퍼런스로 넣어주는 이유는 it의 값을 다른 함수에서도 일괄적으로 움직일 수 있게 하기 위함.
     // server_config의 값을 레퍼런스로 주는 이유는 generateServers에 server_config 변수가 선언되어 있기 때문.
     void parseServerBlock(std::vector<std::string>::iterator& it, std::map<std::string, std::string>& server_config); // 이후에 private로 바꿔주자.
+    void parseServerBlock(std::vector<std::string>::iterator& it, std::map<std::string, std::string>& server_config); 
     void parseLocationBlock(std::vector<std::string>::iterator& it, std::map<std::string, std::string>& server_config);
 };
 
