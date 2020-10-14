f89db28c306a7949bf0a17e6472d21877429c704
iwoo <iwoo@student.42seoul.kr>
2020-10-14 10:01:02 +0900

    MODIFY: split function
    change index type(int -> size_t)
    change method (size() -> length())

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -1,34 +1,22 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   utils.cpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: iwoo <iwoo@student.42seoul.kr>             +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/13 09:51:39 by sanam             #+#    #+#             */
-/*   Updated: 2020/10/13 21:43:43 by iwoo             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "utils.hpp"
 
 namespace ft
 {
 	std::vector<std::string> split(std::string str, std::string const &token)
 	{
-		std::vector<std::string>	result;
-		int							index;
-		std::string 				tmp;
+		std::vector<std::string> result;
+		size_t index;
+		std::string tmp;
 
-		while (str.size())
+		while (str.length())
 		{
 			index = str.find(token);
-			if (static_cast<size_t>(index) != std::string::npos)
+			if (index != std::string::npos)
 			{
 				tmp = str.substr(0, index);
 				if (tmp.length())
 					result.push_back(tmp);
-				str = str.substr(index + token.size());
+				str = str.substr(index + token.length());
 			}
 			else
 			{
