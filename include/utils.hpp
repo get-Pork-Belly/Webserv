3d380d436c7cad87e1b14e360da765f088a8e0e5
l-yohai <yohan9612@naver.com>
2020-10-14 19:20:08 +0900

    TEST: FD_SET OPERATOR MACRO
    Test for fd_set operation macro (fdSet, fdClr, fdIsset, fdZero)

--- include/utils.hpp
+++ include/utils.hpp
@@ -3,12 +3,19 @@
 
 # include <vector>
 # include <string>
+# include <sys/socket.h>
+# include <sys/types.h>
 
 namespace ft
 {
     std::vector<std::string> split(std::string str, std::string const &token);
     std::string	ltrim(const std::string& str, char const *token = "\t\n ");
     std::string	rtrim(const std::string& str, char const *token = "\t\n ");
+
+    void fdZero(fd_set *fds);
+    void fdSet(int fd, fd_set *fds);
+    bool fdIsset(int fd, fd_set *fds);
+    void fdClr(int fd, fd_set *fds);
 }
 
 #endif
