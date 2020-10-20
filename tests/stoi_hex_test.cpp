99355cf93ead84b8a0472429ef85e4d44545eca0
Sanghyeok Nam <sanam@c6r6s4.42seoul.kr>
2020-10-20 19:35:49 +0900

    TEST: stoiHex

---
+++ tests/stoi_hex_test.cpp
@@ -0,0 +1,17 @@
+#include "utils.hpp"
+#include <iostream>
+
+int main()
+{
+	std::string test1 = "abc";
+	std::string test2 = "z123";
+	std::string test3 = "1a87";
+	std::string test4 = "0";
+	std::string test5 = "a";
+
+	std::cout << "Test1: " << ft::stoiHex(test1) << std::endl;
+	std::cout << "Test2: " << ft::stoiHex(test2) << std::endl;
+	std::cout << "Test3: " << ft::stoiHex(test3) << std::endl;
+	std::cout << "Test4: " << ft::stoiHex(test4) << std::endl;
+	std::cout << "Test5: " << ft::stoiHex(test5) << std::endl;
+}
