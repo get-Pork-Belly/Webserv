8d9e47e4022019016858bea222691c7ef23f33e7
Sanghyeok Nam <sanam@c6r6s6.42seoul.kr>
2020-10-28 20:33:13 +0900

    TEST: UrlParserTest

--- Makefile
+++ Makefile
@@ -6,8 +6,7 @@ RM = rm -rf
 DEBUG = -D DEBUG=1
 STDOUT = -D STDOUT=1
 
-# MAIN_FILES = logger_test Log utils ServerManager ServerGenerator Server Response Request
-MAIN_FILES = logger_test Log utils ServerManager ServerGenerator Server Response Request
+MAIN_FILES = url_parser_test UrlParser  utils
 
 SRCS_PATH = $(MAIN_FILES)
 VPATH := .:srcs:tests
---
+++ tests/url_parser_test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include "UrlParser.hpp"
+
+int main()
+{
+    std::string url = "http://www.naver.com/path1/path2/file";
+    std::string noHost= "/path1/path2/file";
+    std::string onlyhost= "www.naver.com";
+    std::string noPaths= "/";
+    std::string folder = "/path1/path2/folder/";
+    std::string file = "/path1/path2/file";
+
+    UrlParser parser;
+    std::cout << "----------------- Url -------------" << std::endl;
+    std::cout << "url: " << url << std::endl;
+    parser.parseUrl(url);
+    parser.clear();
+    std::cout << "----------------- Only host -------------" << std::endl;
+    std::cout << "url: " << onlyhost << std::endl;
+    parser.parseUrl(onlyhost);
+    parser.clear();
+    std::cout << "----------------- noHosts -------------" << std::endl;
+    std::cout << "url: " << noHost << std::endl;
+    parser.parseUrl(noHost);
+    parser.clear();
+    std::cout << "----------------- noPahts -------------" << std::endl;
+    std::cout << "url: " << noPaths<< std::endl;
+    parser.parseUrl(noPaths);
+    parser.clear();
+    std::cout << "----------------- folder -------------" << std::endl;
+    std::cout << "url: " << noPaths<< std::endl;
+    parser.parseUrl(folder);
+    parser.clear();
+    std::cout << "----------------- file -------------" << std::endl;
+    std::cout << "url: " << noPaths<< std::endl;
+    parser.parseUrl(file);
+    parser.clear();
+}
