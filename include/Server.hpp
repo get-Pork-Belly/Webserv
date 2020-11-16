b129a1b60be07ca5c77cdfebbb190c195626b3c3
l-yohai <yohan9612@naver.com>
2020-11-16 16:14:46 +0900

    TEST: for CGI Chunked

--- include/Server.hpp
+++ include/Server.hpp
@@ -18,7 +18,7 @@
 # include "Response.hpp"
 # include "Exception.hpp"
 
-const int BUFFER_SIZE = 500;
+const int BUFFER_SIZE = 65536;
 const int CHUNKED_LINE_LENGTH = 8192;
 const int NUM_OF_META_VARIABLES = 18;
 
@@ -81,7 +81,7 @@ public:
     bool isServerSocket(int fd) const;
     bool isClientSocket(int fd) const;
     bool isStaticResource(int fd) const;
-    // bool isCGIPipe(int fd) const;
+    bool isCGIPipe(int fd) const;
 
     /* Server function */
     void init();
