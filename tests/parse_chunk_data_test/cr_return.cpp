ebc0b38521e35038374749f205f8bee582464582
iwoo <iwoo@student.42seoul.kr>
2020-11-23 16:02:05 +0900

    TEST: parse_chunk_data
    BUFFER_SIZE files which ends with '\r', '\r\n' and 'c'.
    BUFFER_SIZE*2 file which contains '\r' in BUFFER_SIZE index.

---
+++ tests/parse_chunk_data_test/cr_return.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main()
+{
+	int fd;
+
+	fd = open("65536", O_RDWR | O_CREAT, 0666);
+
+	for (int i = 0; i < 65536; i++)
+	{
+		write(fd, "a", 1);
+	}
+	//write(fd, "\r", 1);
+
+	fd = open("65535_cr", O_RDWR | O_CREAT, 0666);
+
+	for (int i = 0; i < 65535; i++)
+	{
+		write(fd, "a", 1);
+	}
+	write(fd, "\r", 1);
+
+	fd = open("65534_crlf", O_RDWR | O_CREAT, 0666);
+
+	for (int i = 0; i < 65534; i++)
+	{
+		write(fd, "a", 1);
+	}
+	write(fd, "\r\n", 2);
+
+	fd = open("65535_cr_65535_cr_65534_crlf", O_RDWR | O_CREAT, 0666);
+
+	for (int i = 0; i < 65534; i++)
+	{
+		write(fd, "a", 1);
+	}
+		write(fd, "A", 1);
+	write(fd, "\r", 1);
+
+	for (int i = 0; i < 65534; i++)
+	{
+		write(fd, "b", 1);
+	}
+		write(fd, "B", 1);
+	write(fd, "\r", 1);
+
+	for (int i = 0; i < 65533; i++)
+	{
+		write(fd, "c", 1);
+	}
+		write(fd, "C", 1);
+	write(fd, "\r\n", 2);
+
+	return (0);
+}
