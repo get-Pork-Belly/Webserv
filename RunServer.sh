fbca569e692a62184db16f5ee6a464598d9060a6
humblEgo <injun.woo30000@gmail.com>
2020-12-23 23:29:12 +0900

    FEAT: make plugin loadable through terminal
    According to the requirements of the subject's bonus part, the shell
    script was changed to determine whether the plug-in should be called
    through the terminal.

--- RunServer.sh
+++ RunServer.sh
@@ -14,7 +14,12 @@ elif [ ${res} -eq ${SERVER_RUN} ]; then
     echo "======================="
     echo ">> PUT CONFIG FILE NAME"
     read config_file_name
-    echo ">>> Servers will run with ${config_file_name}"
+    echo ">>> Servers will run with config flie - ${config_file_name}"
+    echo ">> PUT PLUGIN NAMES"
+    read plugin_names
+    echo ">>> Servers will run with plugins - ${plugin_names}"
+    sed "s/plugin_names/${plugin_names}/" ./config/${config_file_name} > ./config/.temp_config 
     make
-    ./Webserv ./config/${config_file_name}
+    ./Webserv ./config/.temp_config
+    rm ./config/.temp_config
 fi
